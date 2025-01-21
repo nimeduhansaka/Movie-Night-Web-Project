3a6efc8224e1cf023576388900fda77e4bfe88d9
nimeduhansaka <nimedu.h.gbro@gmail.com>
2025-01-21 12:39:09 +0530

    Updated carousel

--- carouselScript.js
+++ carouselScript.js
@@ -17,7 +17,7 @@ class Carousel {
         this.animationID = 0;
 
         this.autoPlayInterval = null;
-        this.autoPlayDelay = 5000; // 4 seconds
+        this.autoPlayDelay = 4000; // 4 seconds
         this.isAutoPlaying = true;
 
         this.init();
